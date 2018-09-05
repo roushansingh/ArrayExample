e9753584e607fbd1caab302ca830f089d35270cc
roushan <roushan83mca@gmail.com>
2018-09-05 21:43:58 +0530

    test message added

--- ArrayExample/ViewController.swift
+++ ArrayExample/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Test branch added
+    //Master branch
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
